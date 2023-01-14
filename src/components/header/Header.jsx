a73493d49a96fb7832c7491adc44f1a6c9d39645
Jisoo <jisoooh0202@gmail.com>
2023-01-14 23:59:16 +0700

    import CTA and HeaderSocials to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const header = () => {
   return (
-	<div>Header</div>
+	<header>
+		<div className="container header__container">
+			<h5>Hello I'm</h5>
+			<h1>Jisoo Oh</h1>
+			<h5 className="text-light">Software Developer & Data Analyst</h5>
+			<CTA />
+			<HeaderSocials />
+
+			<div className="me">
+				<img src={ME} alt="me" />
+			</div>
+
+			<a href="#contact" className='scroll__down'>Contact Me</a>
+		</div>
+	</header>
   )
 }
 
