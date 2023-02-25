95926cd923dff0dc67f5309e8eb3cd6772db8d3a
Jisoo <jisoooh0202@gmail.com>
2023-02-25 22:43:36 +0700

    replace my pictures and resume with demo ones

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import AboutMe from '../../assets/about_me.png'
+import AboutMe from '../../assets/about_me.jpg'
 import { FaAward } from 'react-icons/fa'
 import { FiUsers } from 'react-icons/fi'
 import { VscFolderLibrary } from 'react-icons/vsc'
