e96730b3c881f70d3597f10271f9972f7135b7f8
Jisoo <jisoooh0202@gmail.com>
2023-02-25 22:47:00 +0700

    edit the Header name as the tutorial

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,8 @@ const header = () => {
 		<header>
 			<div className="container header__container">
 				<h5>Hello I'm</h5>
-				<h1>Jisoo Oh</h1>
-				<h5 className="text-light">Software Developer & Data Analyst</h5>
+				<h1>Haijia Bintu</h1>
+				<h5 className="text-light">Fullstack Developer</h5>
 				<CTA />
 				<HeaderSocials />
 
